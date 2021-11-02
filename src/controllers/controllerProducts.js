5dcd75936a5a38755f209da5e0a1aaecb52d5f6d
retamozok <matiasagustin.1823@gmail.com>
2021-11-02 19:38:59 -0300

    - 2/11

---
+++ src/controllers/controllerProducts.js
@@ -0,0 +1,5 @@
+export const nombreFuncion = async (req,res)=>{
+  
+    res.status(200).render('nombre_pagina_hbs')
+  }
+
---
+++ src/routes/routesProducts.js
@@ -0,0 +1,6 @@
+import * as controllerProducts from '../controllers/controllerProducts.js'
+
+const routesProducts = app => {
+    app.get('/products',controllerProducts.nombreFuncion)
+}
+export default routesProducts
